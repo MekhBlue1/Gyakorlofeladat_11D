da0503d94a39f104b0140db1de51b8303b430cbd
Istvan Madarasz <madarasz.istvan@pataky.hu>
2021-01-25 21:44:25 +0100

    Fájl hozzáadva + osztálykész

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class Diak
+    {
+        private int evjarat;
+        private char csoport;
+        private string nev;
+
+        public Diak(int evjarat, char csoport, string nev)
+        {
+            this.evjarat = evjarat;
+            this.csoport = csoport;
+            this.nev = nev;
+        }
+        int Evjarat { get => evjarat; set => evjarat = value; }        
+        public char Csoport { get => csoport; set => csoport = value; }
+        public string Nev { get => nev; set => nev = value; }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            string[] nevek = { "Anna", "Panna", "Kata" };
+           
         }
     }
 }
